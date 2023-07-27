f1377ea983e6d9cc0f91e86e0065f68d81ffa272
PashaahsaP <ppsairda@yandex.ru>
2023-07-27 18:55:18 +0700

    Linq init

---
+++ Linq/ExtensionsTask.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_slideviews;
+
+public static class ExtensionsTask
+{
+	/// <summary>
+	/// Медиана списка из нечетного количества элементов — это серединный элемент списка после сортировки.
+	/// Медиана списка из четного количества элементов — это среднее арифметическое 
+    /// двух серединных элементов списка после сортировки.
+	/// </summary>
+	/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
+	public static double Median(this IEnumerable<double> items)
+    {
+        int count = 0;
+
+        List<double> sortedList = new List<double>();
+
+        foreach (var item in items)
+        {
+            sortedList.Add(item);
+            count++;
+        }
+        if (count > 0)
+        {
+            bool even = count % 2 == 0;
+
+            sortedList.Sort();
+
+            return !even ?
+                sortedList.ElementAt(count / 2) :
+                (sortedList.ElementAt(count / 2) + sortedList.ElementAt((count / 2) - 1)) / 2.0;
+        }
+        else throw new InvalidOperationException();
+
+    }
+
+    /// <returns>
+    /// Возвращает последовательность, состоящую из пар соседних элементов.
+    /// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) and (2,3).
+    /// </returns>
+    public static IEnumerable<(T First, T Second)> Bigrams<T>(this IEnumerable<T> items)
+    {
+        var temp = default(T);
+        bool flag = false;
+        foreach(var item in items)
+            if (flag)
+            {
+                yield return (temp, item);
+                temp = item;
+            }
+            else
+            {
+                flag = true;
+                temp = item;
+            }
+    }
+}
+
+ 
---
+++ Linq/ParsingTask.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace linq_slideviews;
+
+public class ParsingTask
+{
+    private static string[] typeSlides = new[] { "theory", "exercise", "quiz" };
+    /// <param name="lines">все строки файла, которые нужно распарсить. Первая строка заголовочная.</param>
+    /// <returns>Словарь: ключ — идентификатор слайда, значение — информация о слайде</returns>
+    /// <remarks>Метод должен пропускать некорректные строки, игнорируя их</remarks>
+
+    public static IDictionary<int, SlideRecord> ParseSlideRecords(IEnumerable<string> lines)
+    {
+        return lines
+            .Skip(1)
+            .Select(x => x.Split(';'))
+            .Select(x => ParseSlide(x))
+            .Where(x => x != null)
+            .ToDictionary(x => x.Item1, y => new SlideRecord(y.Item1, y.Item2, y.Item3));
+    }
+    
+    /// <param name="lines">все строки файла, которые нужно распарсить. Первая строка — заголовочная.</param>
+    /// <param name="slides">Словарь информации о слайдах по идентификатору слайда. 
+    /// Такой словарь можно получить методом ParseSlideRecords</param>
+    /// <returns>Список информации о посещениях</returns>
+    /// <exception cref="FormatException">Если среди строк есть некорректные</exception>
+    public static IEnumerable<VisitRecord> ParseVisitRecords(
+        IEnumerable<string> lines, IDictionary<int, SlideRecord> slides)
+    {
+        return lines
+            .Skip(1)
+            .Select(x => ParseVisit(x, slides))
+            .Where(x=>x != null);
+
+    }
+
+    #region HelperMethods
+    static VisitRecord ParseVisit(string visit, IDictionary<int, SlideRecord> slides)
+    {
+        var newX = visit.Split(';');
+        try
+        {
+            //if (newX[0] != "" && newX[1] != "" && newX[2] != "" && newX[3] != "")
+                return new VisitRecord(int.Parse(newX[0]), int.Parse(newX[1]),
+                    DateTime.ParseExact($"{newX[2]} {newX[3]}",
+                        "yyyy-MM-dd HH:mm:ss",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None),
+                        slides.FirstOrDefault(x=>x.Key== int.Parse(newX[1])).Value.SlideType);
+        }
+        catch (Exception e)
+        {
+            throw new FormatException($"Wrong line [{visit}]");
+        }
+
+    }
+    static Tuple<int, SlideType, string> ParseSlide(string[] slides)
+    {
+        if (int.TryParse(slides[0], out int id) && slides.Length == 3
+                                                && typeSlides.Any(x => x == slides[1]))
+        {
+            var type = slides[1] switch
+            {
+                "theory" => SlideType.Theory,
+                "exercise" => SlideType.Exercise,
+                "quiz" => SlideType.Quiz,
+            };
+            return Tuple.Create(id, type, slides[2]);
+        }
+        return null;
+    }
+    #endregion
+
+}
