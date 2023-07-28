5616889f270666a162839ea524b82dc6cf372548
PashaahsaP <ppsairda@yandex.ru>
2023-07-28 14:44:30 +0700

    refac

--- Linq/StatisticsTask.cs
+++ Linq/StatisticsTask.cs
@@ -1,31 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace linq_slideviews;
 
 public class StatisticsTask
 {
-	public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
+    public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
     {
-        if(visits.Count == 0) return 0;
+        if (visits.Count == 0) return 0;
         return visits
             .GroupBy(q => q.UserId)
-            .Select(x=>x.OrderBy(x=>x.DateTime)
+            .Select(x => x.OrderBy(x => x.DateTime)
                 .Bigrams())
-            .Select(x => GetTimes(slideType, x))
+            .Select(x => GetTimes(slideType, x, time => time >= 1 && time <= 120))
             .SelectMany(x => x)
-            .Where(time => time >= 1 && time <= 120 )
             .DefaultIfEmpty()
             .Median();
     }
 
-    private static List<double> GetTimes(SlideType slideType, IEnumerable<(VisitRecord First, VisitRecord Second)> x)
+    private static List<double> GetTimes(SlideType slideType, IEnumerable<(VisitRecord First, VisitRecord Second)> x,
+        Func<double, bool> predicate)
     {
         var timeList = new List<double>();
 
         foreach (var pairs in x)
-            if (pairs.First.SlideType == slideType)
-                    timeList.Add((pairs.Second.DateTime - pairs.First.DateTime).TotalMinutes);
+        {
+            var time = (pairs.Second.DateTime - pairs.First.DateTime).TotalMinutes;
+            if (pairs.First.SlideType == slideType && predicate(time))
+                timeList.Add(time);
+        }
 
         return timeList;
     }
